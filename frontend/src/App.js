a3b47be11180723c500619e6b5c1b4fc54c4f35d
Nehul <nehul1313patel@gmail.com>
2022-06-27 22:35:43 +0530

    Setup settings configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,42 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header.js';
-import Search from './components/search.js';
-import {useState} from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header.js";
+import Search from "./components/search.js";
+import { useState } from "react";
 
-const UNSPLASH_KEY = 'dRfgDAQ9bnf3q-h7lQ_SKFzgk7P2uVQjsu2_JzVb-Nk';
+const UNSPLASH_KEY = "dRfgDAQ9bnf3q-h7lQ_SKFzgk7P2uVQjsu2_JzVb-Nk";
 
 function App() {
-  const [word,setWord] = useState('');
+  const [word, setWord] = useState("");
 
-  const handleSearchSubmit = (e) =>{
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then((res) => res.json())
-    .then((data)=>{
-      console.log(data);
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-  }
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    setWord("");
+  };
 
   //console.log(word);
   // console.log(UNSPLASH_KEY);
 
   return (
     <div className="App">
-      <Header title="Images Gallery"></Header>  
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}></Search>
+      <Header title="Images Gallery"></Header>
+
+      <Search
+        word={word}
+        setWord={setWord}
+        handleSubmit={handleSearchSubmit}
+      ></Search>
     </div>
   );
 }
