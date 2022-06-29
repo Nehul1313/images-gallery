54324bea26c85d75746c5f31678cb3d8ebd715c8
Nehul <nehul1313patel@gmail.com>
2022-06-29 20:41:12 +0530

    enabled pylint and black code formatter in python

--- api/main.py
+++ api/main.py
@@ -3,13 +3,14 @@ from flask import Flask, request
 import os
 import requests
 from flask_cors import CORS
-#from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
 
 # load_dotenv(dotenv_path='./.env.local')
-UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
-#UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY","")
-UNSPLASH_KEY = 'dRfgDAQ9bnf3q-h7lQ_SKFzgk7P2uVQjsu2_JzVb-Nk'
-DEBUG = bool(os.environ.get('DEBUG', True))
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+# UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY","")
+UNSPLASH_KEY = "dRfgDAQ9bnf3q-h7lQ_SKFzgk7P2uVQjsu2_JzVb-Nk"
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 
 if not UNSPLASH_KEY:
@@ -18,7 +19,7 @@ if not UNSPLASH_KEY:
 
 app = Flask(__name__)
 CORS(app)
-app.config['DEBUG'] = DEBUG
+app.config["DEBUG"] = DEBUG
 
 
 @app.route("/new-image")
@@ -26,13 +27,11 @@ def new_image():
     word = request.args.get("query")
 
     headers = {
-        'Accept-Version': 'v1',
-        'Authorization': 'Client-ID ' + UNSPLASH_KEY,
+        "Accept-Version": "v1",
+        "Authorization": "Client-ID " + UNSPLASH_KEY,
     }
 
-    params = {
-        'query': word
-    }
+    params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
 
