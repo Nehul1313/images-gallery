30921005a053a8848d59dedd35bb2a9db9a0ce7a
Nehul <nehul1313patel@gmail.com>
2022-07-02 00:14:24 +0530

    introduce axios and rewrite new-image fetch call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import ImageCard from "./components/ImageCard.js";
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Welcome from "./components/Welcome.js";
-// eslint-disable-next-line
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5050";
 const UNSPLASH_KEY = "dRfgDAQ9bnf3q-h7lQ_SKFzgk7P2uVQjsu2_JzVb-Nk";
@@ -15,40 +14,17 @@ function App() {
   const [word, setWord] = useState("");
   const [images, setImages] = useState([]);
 
-  //console.log(images);
-
   const handleSearchSubmit = async (e) => {
     e.preventDefault();
-    //console.log(word);
-    console.log('sending fetch request');
-
-    // fetch(
-    //   `${API_URL}/new-image?query=${word}&client_id=${UNSPLASH_KEY}`
-    // )
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     console.log('adding found image to the state');
-    //     setImages([{ ...data, title: word }, ...images]);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-
     
     try {
       const res = await axios.get(`${API_URL}/new-image?query=${word}&client_id=${UNSPLASH_KEY}`);
-      console.log('adding found image to the state');
       setImages([{ ...res.data, title: word }, ...images]);
     } catch (error) {
       console.log(error);
     }
-
-    console.log('clearing search form');
     setWord("");
   };
-
-  //console.log(word);
-  // console.log(UNSPLASH_KEY);
 
   const handleDeleteImage = (id) => {
     setImages(images.filter((image) => image.id !== id));
